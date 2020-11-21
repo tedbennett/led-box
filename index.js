15513cc3547fe845d5fdb43968926c13021bc269
Ted <teddsbennett@gmail.com>
2020-11-21 08:54:55 +0000

    Add ws message

--- index.js
+++ index.js
@@ -15,14 +15,15 @@ var wss = new WebSocketServer({server: server})
 console.log("websocket server created")
 
 wss.on("connection", function(ws) {
-  var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
-
   console.log("websocket connection open")
+
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+    ws.send(message)
+  });
+
 
   ws.on("close", function() {
     console.log("websocket connection close")
-    clearInterval(id)
   })
 })
